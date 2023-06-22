42af224e6a0ed9f10f5acc7361eae1fa463bcb78
Matthew Chuong <mstchuong@yahoo.com>
2023-06-21 21:52:41 -0400

    use react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-import NavBar from "./components/navbar/NavBar"
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
+import NavBar from "./components/navbar/NavBar"
+import Landing from "./pages/landing/Landing";
 
 function App() {
   return (
     <div className='App'>
-      <NavBar />
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          {/* <Route path="/" element={<Home />} /> */}
+          {/* <Route path="contact" element={<></>} /> */}
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,12 +1,37 @@
-import { AppBar, Toolbar } from "@mui/material";
+import {
+  AppBar, Toolbar, useMediaQuery, useTheme,
+  IconButton,
+  Menu,
+} from "@mui/material";
+import { useNavigate } from "react-router-dom";
 // eslint-disable-next-line import/no-unresolved
 import styles from './NavBar.module.scss';
 
 function NavBar() {
+  const theme = useTheme();
+  const isDesktop = useMediaQuery(theme.breakpoints.up('md'));
+  const navigate = useNavigate();
+
+  const handleNavigate = (path) => {
+    navigate(path);
+  }
+
   return (
-    <AppBar classes={{ root: styles.appBar }}>
+    <AppBar classes={{ root: styles.appBar }} position='sticky'>
       <Toolbar>
         <div>Vietnamese Beallwood Baptist Church</div>
+        {
+          isDesktop
+          ? (
+            <div>
+              <span onClick={() => handleNavigate('/')} aria-hidden='true'>Home</span>
+            </div>
+          ) : (
+            <IconButton>
+              <Menu style={{ color: 'black' }}/>
+            </IconButton>
+          )
+        }
       </Toolbar>
     </AppBar>
   )
